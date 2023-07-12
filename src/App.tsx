5be027bc381f19b6f96192ef5695383bb2cc81cb
belclei <belclei@gmail.com>
2023-07-11 22:30:34 -0300

    just started

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './global.css'
+
+
+export function App() {
+
+  return (
+    <>
+      <h1 className='header'>Vite + React</h1>
+      <div className="card">
+        <button>
+          count is
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
